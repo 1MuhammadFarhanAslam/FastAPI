43f2bca07b3a72fb2b7d142c9f2465567e09f885
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-02-26 17:43:45 +0500

    Updated tts_api.py

--- app/end_points/tts_api.py
+++ app/end_points/tts_api.py
@@ -1,3 +1,4 @@
+import bittensor as bt
 from classes.tts import TextToSpeechService
 import torch
 import random
@@ -10,38 +11,49 @@ class TTS_API(TextToSpeechService):
 
     def get_filtered_axons(self):
         # Get the uids of all miners in the network.
-        uids = self.metagraph.uids.tolist()
-        queryable_uids = (self.metagraph.total_stake >= 0)
-        # Remove the weights of miners that are not queryable.
-        queryable_uids = queryable_uids * torch.Tensor([self.metagraph.neurons[uid].axon_info.ip != '0.0.0.0' for uid in uids])
-        queryable_uid = queryable_uids * torch.Tensor([
-            any(self.metagraph.neurons[uid].axon_info.ip == ip for ip in lib.BLACKLISTED_IPS) or
-            any(self.metagraph.neurons[uid].axon_info.ip.startswith(prefix) for prefix in lib.BLACKLISTED_IPS_SEG)
-            for uid in uids
-        ])
-        active_miners = torch.sum(queryable_uids)
-        dendrites_per_query = self.total_dendrites_per_query
-
-        # if there are no active miners, set active_miners to 1
-        if active_miners == 0:
-            active_miners = 1
-        # if there are less than dendrites_per_query * 3 active miners, set dendrites_per_query to active_miners / 3
-        if active_miners < self.total_dendrites_per_query * 3:
-            dendrites_per_query = int(active_miners / 3)
-        else:
+        try:
+            uids = self.metagraph.uids.tolist()
+            queryable_uids = (self.metagraph.total_stake >= 0)
+            # Remove the weights of miners that are not queryable.
+            queryable_uids = queryable_uids * torch.Tensor([self.metagraph.neurons[uid].axon_info.ip != '0.0.0.0' for uid in uids])
+            queryable_uid = queryable_uids * torch.Tensor([
+                any(self.metagraph.neurons[uid].axon_info.ip == ip for ip in lib.BLACKLISTED_IPS) or
+                any(self.metagraph.neurons[uid].axon_info.ip.startswith(prefix) for prefix in lib.BLACKLISTED_IPS_SEG)
+                for uid in uids
+            ])
+            bt.logging.debug(f"Queryable uids in fastapi: {queryable_uids}")
+        except Exception as e:
+            print(f"An error occurred while filtering queryable uids in fastapi: {e}")
+        try:
+            active_miners = torch.sum(queryable_uids)
             dendrites_per_query = self.total_dendrites_per_query
-        
+
+            # if there are no active miners, set active_miners to 1
+            if active_miners == 0:
+                active_miners = 1
+            # if there are less than dendrites_per_query * 3 active miners, set dendrites_per_query to active_miners / 3
+            if active_miners < self.total_dendrites_per_query * 3:
+                dendrites_per_query = int(active_miners / 3)
+            else:
+                dendrites_per_query = self.total_dendrites_per_query
+        except Exception as e:
+            print(f"An error occurred while dendrites per query calculation in fastapi : {e}")        
         # less than 3 set to 3
-        if dendrites_per_query < self.minimum_dendrites_per_query:
+        try:
+            if dendrites_per_query < self.minimum_dendrites_per_query:
                 dendrites_per_query = self.minimum_dendrites_per_query
-        # zip uids and queryable_uids, filter only the uids that are queryable, unzip, and get the uids
-        zipped_uids = list(zip(uids, queryable_uids))
-        zipped_uid = list(zip(uids, queryable_uid))
-        filtered_zipped_uids = list(filter(lambda x: x[1], zipped_uids))
-        filtered_uids = [item[0] for item in filtered_zipped_uids] if filtered_zipped_uids else []
-        filtered_zipped_uid = list(filter(lambda x: x[1], zipped_uid))
-        filtered_uid = [item[0] for item in filtered_zipped_uid] if filtered_zipped_uid else []
-        self.filtered_axon = filtered_uid
+            # zip uids and queryable_uids, filter only the uids that are queryable, unzip, and get the uids
+            zipped_uids = list(zip(uids, queryable_uids))
+            zipped_uid = list(zip(uids, queryable_uid))
+            filtered_zipped_uids = list(filter(lambda x: x[1], zipped_uids))
+            filtered_uids = [item[0] for item in filtered_zipped_uids] if filtered_zipped_uids else []
+            filtered_zipped_uid = list(filter(lambda x: x[1], zipped_uid))
+            filtered_uid = [item[0] for item in filtered_zipped_uid] if filtered_zipped_uid else []
+            self.filtered_axon = filtered_uid
+            bt.logging.info(f"Filtered axons in fastapi: {self.filtered_axon}")
+        except Exception as e:
+            print(f"An error occurred while filtering axons in fastapi: {e}")
         return self.filtered_axon
+
     
 
