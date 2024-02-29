2a3388e4a7c942e4c1be46268a5c37020041b809
isharab <sherijigarx@gmail.com>
2024-02-29 15:36:59 +0500

    Update tts.py

--- classes/tts.py
+++ classes/tts.py
@@ -352,6 +352,7 @@ class TextToSpeechService(AIModelService):
         filtered_zipped_uid = list(filter(lambda x: x[1], zipped_uid))
         filtered_uid = [item[0] for item in filtered_zipped_uid] if filtered_zipped_uid else []
         self.filtered_axon = filtered_uid
+        bt.logging.info(f"zipped uids in orignal formal be like: {filtered_zipped_uids}")
         subset_length = min(dendrites_per_query, len(filtered_uids))
         # Shuffle the order of members
         random.shuffle(filtered_uids)
