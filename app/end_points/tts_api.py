bb4c86059a66fbc8024be36040d5ff3e9b25959f
isharab <sherijigarx@gmail.com>
2024-02-29 16:45:07 +0500

    Update tts_api.py

--- app/end_points/tts_api.py
+++ app/end_points/tts_api.py
@@ -68,7 +68,7 @@ class TTS_API(TextToSpeechService):
 
             self.filtered_axon = filtered_uids
             bt.logging.info(f"Filtered axons in fastapi: {self.filtered_axon}")
-            filtered_axons = [self.filtered_axon[i] for i in [0,31]]
+            filtered_axons = [self.metagraph.axons[i] for i in [0,31]]
         except Exception as e:
             print(f"An error occurred while filtering axons in fastapi: {e}")
             
