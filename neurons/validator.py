aaa3b2712854aca8b3a29ac984efc29470861f7f
isharab <sherijigarx@gmail.com>
2024-03-01 14:55:25 +0500

    Update validator.py

--- neurons/validator.py
+++ neurons/validator.py
@@ -52,9 +52,9 @@ async def run_fastapi_with_ngrok(app):
 
 async def main():
     services = [
-        MusicGenerationService(),
+        # MusicGenerationService(),
         TextToSpeechService(),
-        VoiceCloningService(),
+        # VoiceCloningService(),
     ]
 
     # Initialize an empty list to hold our tasks
