9867f644b53c0f201ce524b030d9838fb2553f43
isharab <sherijigarx@gmail.com>
2024-03-02 22:42:05 +0500

    Update user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -231,7 +231,10 @@ async def vc_service(prompt: str = Form(...),  audio_file: Optional[UploadFile] 
                 logging.error(f"the generate_voice_clone functions is not being called due to the error with {e}")
 
             # Process the response
-            audio_data = vc_api.handle_clone_output(axon, response)
+            try:
+                audio_data = vc_api.handle_clone_output(axon, response)
+            except Exception as e:
+                logging.error(f"the handle_clone_output functions is not being called due to the error with {e}")
             bt.logging.info(f" ____________________________________ Audio data ____________________________________ : {audio_data}")
 
             file_extension = os.path.splitext(audio_data)[1].lower()
