39e90b9888baae8df45e37f56081d40ba2ab391d
isharab <sherijigarx@gmail.com>
2024-03-02 20:42:52 +0500

    Update user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -224,7 +224,7 @@ async def vc_service(request: str = Form(...),  audio_file: Optional[UploadFile]
             # Use the prompt from the request in the query_network function
             bt.logging.info(f"request prompt: {request}")
             bt.logging.info(f"request axon here: {axon}")
-            response = vc_api.generate_voice_clone(text_input=request, clone_input=input_audio, sample_rate=sample_rate, api_axon=axon)
+            response = vc_api.generate_voice_clone(text_input=request, clone_input=input_audio, sample_rate=sample_rate)
             bt.logging.info(f"____________________________________ response ____________________________________ : {response}")
 
             # Process the response
