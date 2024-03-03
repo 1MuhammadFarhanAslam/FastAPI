539eede7e94365c2fb397c71d2b9afdec1541048
isharab <sherijigarx@gmail.com>
2024-03-03 16:54:32 +0500

    Update vc.py

--- classes/vc.py
+++ classes/vc.py
@@ -272,6 +272,7 @@ class VoiceCloningService(AIModelService):
                 torchaudio.save(cloned_file_path, src=audio_data_int, sample_rate=sampling_rate)
                 bt.logging.info(f"the cloned file have been saved successfully: {cloned_file_path}")
                 score = self.score_output(input_file, cloned_file_path, prompt) # self.audio_file_path
+                bt.logging.info(f"the score of the cloned file issssssssssssssssssss: {score}")
                 self.update_score(axon, score, service="Voice Cloning", ax=axon)
                 # existing_wav_files = [f for f in os.listdir('/tmp') if f.endswith('.wav')]
                 # for existing_file in existing_wav_files:
