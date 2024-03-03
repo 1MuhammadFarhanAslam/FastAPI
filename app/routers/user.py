3e8b62ebfe0882b7f75d78c0f79cf55291207eb2
isharab <sherijigarx@gmail.com>
2024-03-03 16:43:54 +0500

    Update user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -226,7 +226,7 @@ async def vc_service(prompt: str = Form(...),  audio_file: Optional[UploadFile] 
             bt.logging.info(f"prompt axon here: {axon}")
 
             try:
-                audio_data = await vc_api.generate_voice_clone(prompt, input_audio, sample_rate, api_axon=axon)
+                audio_data = await vc_api.generate_voice_clone(prompt, input_audio, sample_rate, api_axon=filtered_axons)
             except Exception as e:
                 logging.error(f"the generate_voice_clone functions is not being called due to the error with {e}")
 
