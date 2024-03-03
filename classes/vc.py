549ed5b863f227efb3fb0998aaefb3d9c1f7ce5e
isharab <sherijigarx@gmail.com>
2024-03-03 22:25:38 +0500

    Update vc.py

--- classes/vc.py
+++ classes/vc.py
@@ -227,11 +227,11 @@ class VoiceCloningService(AIModelService):
             if self.response is not None and isinstance(self.response, lib.protocol.VoiceClone) and self.response.clone_output is not None and self.response.dendrite.status_code == 200:
                 bt.logging.success(f"Received Voice Clone output from {ax.hotkey}")
                 vc_file = self.handle_clone_output(ax, self.response, prompt=text_input, input_file=input_file)
+                return ax.hotkey, vc_file
             elif self.response.dendrite.status_code != 403:
                 self.punish(ax, service="Voice Cloning", punish_message=self.response.dendrite.status_message)
             else:
                 pass
-            return ax.hotkey, vc_file
         except Exception as e:
             print(f"An error occurred while processing voice clone responses: {e}")
 
