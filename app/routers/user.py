34871baeb3f012868cb0abfc76d28b8abf7d1139
shaukathussain <33412791+IamHussain503@users.noreply.github.com>
2024-03-05 00:20:18 +0100

    Update user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -224,7 +224,7 @@ async def vc_service(prompt: str = Form(...),  audio_file: Optional[UploadFile] 
             audio_data = None  # Define audio_data outside try-except scope
 
             try:
-                audio_data = await vc_api.generate_voice_clone(prompt, input_audio, sample_rate, api_axon=list(axon), input_file=temp_file_path)
+                audio_data = await vc_api.generate_voice_clone(prompt, input_audio, sample_rate, api_axon=[axon], input_file=temp_file_path)
             except Exception as e:
                 logging.error(f"the generate_voice_clone functions is not being called due to the error with {e}")
             file_extension = os.path.splitext(audio_data)[1].lower()
